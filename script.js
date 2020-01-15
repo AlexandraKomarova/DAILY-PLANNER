fd02641d50ac076990cdb2516b0e8aa9b4f4d32c
Sonechka M <s212288s@gmail.com>
2020-01-14 22:02:38 -0500

    saving to local storage and getting information to render/

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
-var submit = document.querySelector("#submit")
+$(document).ready(function() {
+  var todoDiv = document.querySelector(".to-do-div");
+  var todoInput = document.querySelector("#todo-text");
+  var submit = document.querySelector(".submit")
 
-submit.addEventListener("submit", function(){
-    console.log("submitted")
-})
+  var todos = []
 
-var toDo = localStorage.getItem("toDo");
+  $(".submit").on("click", function(){
+    //console.log($(this).siblings('.todo-text').val());
+    //console.log($(this).parent().attr('id'));
 
-var todoField = document.querySelector(".to-do-field")
+    var time = $(this).parent().attr('id');
+    var value = $(this).siblings('.todo-text').val();
 
-localStorage.setItem("toDo", todoField.value)
+    localStorage.setItem(time, value);
+
+    console.log(localStorage);
+  });
+});
+
+
+$("#9-am .todo-text").val(localStorage.getItem("9-am"));
+$("#10-am .todo-text").val(localStorage.getItem("10-am"));
+$("#11-am .todo-text").val(localStorage.getItem("11-am"));
+$("#12-pm .todo-text").val(localStorage.getItem("12-pm"));
+$("#1-pm .todo-text").val(localStorage.getItem("1-pm"));
+$("#2-pm .todo-text").val(localStorage.getItem("2-pm"));
+$("#3-pm .todo-text").val(localStorage.getItem("3-pm"));
+$("#4-pm .todo-text").val(localStorage.getItem("4-pm"));
+$("#5-pm .todo-text").val(localStorage.getItem("5-pm"));
